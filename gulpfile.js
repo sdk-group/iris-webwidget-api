e28c130f15d5cb112630ff498a4d997f4d26e14b
artificial-life <proteus.protocol@gmail.com>
2016-06-15 19:15:01 +0600

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
 
 gulp.task('default', () => {
-	return gulp.src('src/iris-webterminal.js')
+	return gulp.src('src/**/*.js')
 		.pipe(babel({
 			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('build'))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
 		}))
 		.pipe(gulp.dest('build'));
 });
